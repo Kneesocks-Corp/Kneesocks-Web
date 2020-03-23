582fae43f177467143094ecff2b1c759a0595495
LordDamionDevil <robloxfilmcam@gmail.com>
2020-03-23 12:56:31 +0100

    Fix now and get.php

--- api/get.php
+++ api/get.php
@@ -7,12 +7,14 @@ error_reporting(0);
 if($type === "high" and $mode === "RKS"){
 	$remoteImage = "https://www.elsetge.cat/myimg/f/131-1316976_anime-girl-in-knee-high-socks.png";
 	if($format === "json"){
+		header('Content-Type: application/json');
 	    $output['url'] = $remoteImage;
 		echo json_encode($output, JSON_PRETTY_PRINT);
+	}elseif($format === "img"){
+		$imginfo = getimagesize($remoteImage);
+		header("Content-type: {$imginfo['mime']}");
+		readfile($remoteImage);
 	}
-	$imginfo = getimagesize($remoteImage);
-	header("Content-type: {$imginfo['mime']}");
-	readfile($remoteImage);
 }elseif($type === "low" and $mode === "RKS"){
     $output['type'] = $type;
     echo json_encode($output, JSON_PRETTY_PRINT);
