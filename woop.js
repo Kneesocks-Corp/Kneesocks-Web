19a850163d5270838e81596f135f614c0dff1d78
LordDamionDevil <robloxfilmcam@gmail.com>
2020-03-23 13:27:57 +0100

    Update woop.js

--- woop.js
+++ woop.js
@@ -2,14 +2,12 @@ var express = require("express");
 var path = require("path");
 var port = process.env.PORT || 3000;
 var app = express();
-app.use(app.router);
 app.get("/", function (req, res) {
  res.sendFile(path.join(__dirname, '/index.html'));
 });
 app.use('/css', express.static(path.join(__dirname, '/css')));
 app.use('/js', express.static(path.join(__dirname, '/js')));
 app.use(function(req, res, next){
-  res.status(404);
-  res.sendFile(path.join(__dirname, '/404.html'));
+  res.status(404).sendFile(path.join(__dirname, '/404.html'));
 });
 app.listen(port, function () {});
