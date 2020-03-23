f888260f474f94ceff2eb5eff8be98013ab797a5
LordDamionDevil <robloxfilmcam@gmail.com>
2020-03-23 19:24:55 +0100

    Update woop.js

--- woop.js
+++ woop.js
@@ -8,34 +8,15 @@ app.get("/", function (req, res) {
 app.use('/css', express.static(path.join(__dirname, '/css')));
 app.use('/js', express.static(path.join(__dirname, '/js')));
 app.get('/corona', function(req, res) {
-  const path = path.join(__dirname, '/ch.mp4')
-  const stat = fs.statSync(path)
+  const pathy = path.join(__dirname, '/ch.mp4')
+  const stat = fs.statSync(pathy)
   const fileSize = stat.size
-  const range = req.headers.range
-  if (range) {
-    const parts = range.replace(/bytes=/, "").split("-")
-    const start = parseInt(parts[0], 10)
-    const end = parts[1] 
-      ? parseInt(parts[1], 10)
-      : fileSize-1
-    const chunksize = (end-start)+1
-    const file = fs.createReadStream(path, {start, end})
-    const head = {
-      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunksize,
-      'Content-Type': 'video/mp4',
-    }
-    res.writeHead(206, head);
-    file.pipe(res);
-  } else {
-    const head = {
-      'Content-Length': fileSize,
-      'Content-Type': 'video/mp4',
-    }
-    res.writeHead(200, head)
-    fs.createReadStream(path).pipe(res)
+  const head = {
+    'Content-Length': fileSize,
+    'Content-Type': 'video/mp4',
   }
+  res.writeHead(200, head)
+  fs.createReadStream(pathy).pipe(res)
 });
 app.use(function(req, res, next){
   res.status(404).sendFile(path.join(__dirname, '/404.html'));
