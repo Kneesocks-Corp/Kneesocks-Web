b3980bc1049cfa2c5aea4c6b1f369affc5ec008f
LordDamionDevil <robloxfilmcam@gmail.com>
2020-03-23 19:28:23 +0100

    Update woop.js

--- woop.js
+++ woop.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var path = require("path");
-var fs = require("fs");
 var port = process.env.PORT || 3000;
 var app = express();
 app.get("/", function (req, res) {
@@ -9,15 +8,7 @@ app.get("/", function (req, res) {
 app.use('/css', express.static(path.join(__dirname, '/css')));
 app.use('/js', express.static(path.join(__dirname, '/js')));
 app.get('/corona', function(req, res) {
-  const pathy = path.join(__dirname, '/ch.mp4')
-  const stat = fs.statSync(pathy)
-  const fileSize = stat.size
-  const head = {
-    'Content-Length': fileSize,
-    'Content-Type': 'video/mp4',
-  }
-  res.writeHead(200, head)
-  fs.createReadStream(pathy).pipe(res)
+  res.sendFile(path.join(__dirname, '/ch.mp4'));
 });
 app.use(function(req, res, next){
   res.status(404).sendFile(path.join(__dirname, '/404.html'));
