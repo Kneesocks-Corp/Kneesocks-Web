5fc4cebfee28bbb55049bd108ac18fdb21211aec
LordDamionDevil <robloxfilmcam@gmail.com>
2020-03-23 19:04:16 +0100

    Update woop.js

--- woop.js
+++ woop.js
@@ -9,31 +9,11 @@ app.get("/", function (req, res) {
 });
 app.get('/corona', function(req, res) {
   const path = request('https://dedibox.tech/ch.mp4').pipe(fs.createWriteStream('ch.mp4'));
-  const stat = fs.statSync(path)
-  const fileSize = stat.size
-  const range = req.headers.range
-  if (range) {
-    const parts = range.replace(/bytes=/, "").split("-")
-    const start = parseInt(parts[0], 10)
-    const end = parts[1] 
-      ? parseInt(parts[1], 10)
-      : fileSize-1
-    const chunksize = (end-start)+1
-    const file = fs.createReadStream(path, {start, end})
-    const head = {
-      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunksize,
+  const head = {
       'Content-Type': 'video/mp4',
-    }
-    res.writeHead(206, head);
-    file.pipe(res);
-  } else {
-    const head = {
-      'Content-Type': 'video/mp4',
-    }
-    res.writeHead(200, head)
-    fs.createReadStream(path).pipe(res)
+  }
+  res.writeHead(200, head)
+  path.pipe(res)
   }
 });
 app.use('/css', express.static(path.join(__dirname, '/css')));
