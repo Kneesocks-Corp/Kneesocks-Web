342532fd7a45f5dd1140a4094e167eda1fdeb74e
LordDamionDevil <robloxfilmcam@gmail.com>
2020-03-23 12:43:05 +0100

    Change get and index

--- api/get.php
+++ api/get.php
@@ -1,14 +1,15 @@
 <?php
+$mode = $_GET['mode'];
 $type = $_GET['type'];
 error_reporting(0);
-header('Content-Type: application/json');
-if($type === "Upper"){
-    $output['type'] = $type;
-    echo json_encode($output, JSON_PRETTY_PRINT);
-}elseif($type === "Lower"){
+// MongoDB wird bald noch hinzugefügt für random pics und etc
+if($type === "Upper" and $mode === "RKS"){
+    echo file_get_contents('https://www.elsetge.cat/myimg/f/131-1316976_anime-girl-in-knee-high-socks.png');
+}elseif($type === "Lower" and $mode === "RKS"){
     $output['type'] = $type;
     echo json_encode($output, JSON_PRETTY_PRINT);
 }else{
+	header('Content-Type: application/json');
     $output['type'] = "N/A";
     echo json_encode($output, JSON_PRETTY_PRINT);
 }
