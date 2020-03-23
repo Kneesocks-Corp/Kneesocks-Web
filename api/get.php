f49432bf889bc00b3c6aaa14d9f713cfb5440821
LordDamionDevil <robloxfilmcam@gmail.com>
2020-03-23 12:47:14 +0100

    Fix image not showing

--- api/get.php
+++ api/get.php
@@ -4,7 +4,10 @@ $type = $_GET['type'];
 error_reporting(0);
 // MongoDB wird bald noch hinzugefügt für random pics und etc
 if($type === "Upper" and $mode === "RKS"){
-    echo file_get_contents('https://www.elsetge.cat/myimg/f/131-1316976_anime-girl-in-knee-high-socks.png');
+	$remoteImage = "https://www.elsetge.cat/myimg/f/131-1316976_anime-girl-in-knee-high-socks.png";
+	$imginfo = getimagesize($remoteImage);
+	header("Content-type: {$imginfo['mime']}");
+	readfile($remoteImage);
 }elseif($type === "Lower" and $mode === "RKS"){
     $output['type'] = $type;
     echo json_encode($output, JSON_PRETTY_PRINT);
