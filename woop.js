7e17c8c385f4b977ab10be607de2ef270349c070
LordDamionDevil <robloxfilmcam@gmail.com>
2020-03-23 19:02:20 +0100

    Update woop.js

--- woop.js
+++ woop.js
@@ -1,13 +1,40 @@
 var express = require("express");
 var path = require("path");
 var request = require("request");
+var fs = require("fs");
 var port = process.env.PORT || 3000;
 var app = express();
 app.get("/", function (req, res) {
  res.sendFile(path.join(__dirname, '/index.html'));
 });
 app.get('/corona', function(req, res) {
-    request('https://dedibox.tech/ch.mp4').pipe(res);
+  const path = request('https://dedibox.tech/ch.mp4').pipe(fs.createWriteStream('ch.mp4'));
+  const stat = fs.statSync(path)
+  const fileSize = stat.size
+  const range = req.headers.range
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-")
+    const start = parseInt(parts[0], 10)
+    const end = parts[1] 
+      ? parseInt(parts[1], 10)
+      : fileSize-1
+    const chunksize = (end-start)+1
+    const file = fs.createReadStream(path, {start, end})
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(200, head)
+    fs.createReadStream(path).pipe(res)
+  }
 });
 app.use('/css', express.static(path.join(__dirname, '/css')));
 app.use('/js', express.static(path.join(__dirname, '/js')));
