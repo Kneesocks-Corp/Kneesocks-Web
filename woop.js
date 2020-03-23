bba4e1d86a6304e6d0f65e72de667cc5e8e22e31
LordDamionDevil <robloxfilmcam@gmail.com>
2020-03-23 18:57:05 +0100

    syntax error

--- woop.js
+++ woop.js
@@ -13,7 +13,6 @@ app.get('/corona', function(req, res) {
     res.writeHead(200, head)
     var epic = request('https://dedibox.tech/ch.mp4').pipe(fs.createWriteStream('ch.mp4'))
 	epic.pipe(res);
-  }
 });
 app.use('/css', express.static(path.join(__dirname, '/css')));
 app.use('/js', express.static(path.join(__dirname, '/js')));
