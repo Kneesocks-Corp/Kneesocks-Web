651c863ea74999df4422edaf1679aa7678ab9731
LordDamionDevil <robloxfilmcam@gmail.com>
2020-03-23 19:00:25 +0100

    Update woop.js

--- woop.js
+++ woop.js
@@ -7,12 +7,7 @@ app.get("/", function (req, res) {
  res.sendFile(path.join(__dirname, '/index.html'));
 });
 app.get('/corona', function(req, res) {
-    const head = {
-      'Content-Type': 'video/mp4',
-    }
-    res.writeHead(200, head)
-    var epic = request('https://dedibox.tech/ch.mp4').pipe(fs.createWriteStream('ch.mp4'))
-	epic.pipe(res);
+    request('https://dedibox.tech/ch.mp4').pipe(res);
 });
 app.use('/css', express.static(path.join(__dirname, '/css')));
 app.use('/js', express.static(path.join(__dirname, '/js')));
