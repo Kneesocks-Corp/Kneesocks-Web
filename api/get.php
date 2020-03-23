41a8aa1d2d722c884368cf815ff360be28da01f7
LordDamionDevil <robloxfilmcam@gmail.com>
2020-03-23 12:52:09 +0100

    add format

--- api/get.php
+++ api/get.php
@@ -1,14 +1,19 @@
 <?php
 $mode = $_GET['mode'];
 $type = $_GET['type'];
+$format = $_GET['format'];
 error_reporting(0);
 // MongoDB wird bald noch hinzugefügt für random pics und etc
-if($type === "Upper" and $mode === "RKS"){
+if($type === "high" and $mode === "RKS"){
 	$remoteImage = "https://www.elsetge.cat/myimg/f/131-1316976_anime-girl-in-knee-high-socks.png";
+	if($format === "json"){
+	    $output['url'] = $remoteImage;
+		echo json_encode($output, JSON_PRETTY_PRINT);
+	}
 	$imginfo = getimagesize($remoteImage);
 	header("Content-type: {$imginfo['mime']}");
 	readfile($remoteImage);
-}elseif($type === "Lower" and $mode === "RKS"){
+}elseif($type === "low" and $mode === "RKS"){
     $output['type'] = $type;
     echo json_encode($output, JSON_PRETTY_PRINT);
 }else{
