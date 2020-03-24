0041049e191f0f634d043ed073b0076c2e663383
LordDamionDevil <robloxfilmcam@gmail.com>
2020-03-24 18:02:47 +0100

    Update woop.js

--- woop.js
+++ woop.js
@@ -57,6 +57,37 @@ app.get("/api/v2/:mode-:type", function (req, res) {
 	}
  };
 });
+app.get("/api/v2/:mode", function (req, res) {
+ var collecta = [];
+ if(req.params.mode.split('.')[0] == "RKS"){
+	mongodb.connect(constring, { useUnifiedTopology: true, useNewUrlParser: true }, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("kneesocks");
+      dbo.collection("imgs").find({}).forEach(function(row) {
+        var getdad = {};
+        getdad["data"] = row.imgdata;
+        getdad["type"] = row.type;
+        collecta.push(getdad);
+        db.close();
+      }, done);
+    });
+ }else {
+	res.send("Invalid Arguments");
+ }
+ function done(err){
+	if(collecta.length == 0){res.send("Invalid Arguments");};
+	var tab = "";
+	if(req.params.mode.split('.')[0] == "RKS"){
+	  tab = collecta[collecta.length * Math.random() | 0];
+	};
+	if(req.params.type.split('.')[1] == "json"){
+	  res.json(tab);
+	}else {
+	  var img = Buffer.from(tab.data.split(',')[1], 'base64');
+	  res.end(img);
+	}
+ };
+});
 app.use(function(req, res, next){
   res.status(404).sendFile(path.join(__dirname, '/404.html'));
 });
